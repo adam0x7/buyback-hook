0b4f84bfdccbeb97b8c7f70158084d3c9227ca40
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-10-19 13:45:11 -0500

    pushing current script up

--- script/Treasury.s.sol
+++ script/Treasury.s.sol
@@ -4,6 +4,7 @@
 pragma solidity ^0.8.19;
 
 import {Script, console2} from "forge-std/Script.sol";
+
 import { Treasury } from "../src/Treasury.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {Counter} from "../src/Counter.sol";
@@ -18,6 +19,7 @@ import {Currency, CurrencyLibrary} from "@uniswap/v4-core/contracts/types/Curren
 import {ICounter} from "../src/ICounter.sol";
 import {IHooks} from "@uniswap/v4-core/contracts/interfaces/IHooks.sol";
 import {Currency, CurrencyLibrary} from "@uniswap/v4-core/contracts/types/Currency.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TreasuryScript is Script, Deployers {
     address constant CREATE2_DEPLOYER = address(0x13b0D85CcB8bf860b6b79AF3029fCA081AE9beF2); // Mumbai Deployer from -> https://github.com/pcaversaccio/create2deployer
@@ -34,29 +36,13 @@ contract TreasuryScript is Script, Deployers {
         PoolModifyPositionTest router =
         new PoolModifyPositionTest(IPoolManager(address(0x5FF8780e4D20e75B8599A9C4528D8ac9682e5c89)));
 
-        // deploying some mock tokens
-        MockERC20 _tokenA = MockERC20(0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97);
-        MockERC20 _tokenB = MockERC20(0x4f81Ff288518727Ae2583f67fEDb46533c9F1238);
-        MockERC20 token0;
-        MockERC20 token1;
+        // Token addresses that I deployed on my own beforehand
+        IERC20 token0 = IERC20(0x2EFf4B88Cfa00180409A5cF2f248CBF6Af4e22D6);
+        IERC20 token1 = IERC20(0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97);
 
-
-        //token1 can't be longer than token0, we need to make sure that token0 is smaller
-        if (address(_tokenA) < address(_tokenB)) {
-            token0 = _tokenA;
-            token1 = _tokenB;
-        } else {
-            token0 = _tokenB;
-            token1 = _tokenA;
-        }
-
-        // mint tokens to sender
-        token0.mint(msg.sender, 1000e18);
-        token1.mint(msg.sender, 1000e18);
-
-        token0.approve(address(router), 1000e18);
-        token1.approve(address(router), 1000e18);
-
+        //approving router to spend tokens to add liquidity later
+        token0.approve(address(router), 10);
+        token1.approve(address(router), 1000);
 
         //setting the flag for the hook that we will be using
         uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
@@ -65,11 +51,7 @@ contract TreasuryScript is Script, Deployers {
         (address hookAddress, bytes32 salt) =
                             HookMiner.find(CREATE2_DEPLOYER, flags, 3000, type(Counter).creationCode,
                             abi.encode(address(manager)));
-
-
-        // Deploying the hook using a CREATE2 address that I found on Mumbai, probably will deploy another soon
-        vm.broadcast();
-        Counter counter = new Counter{salt: salt}(manager);
+        Counter counter = new Counter{salt: salt}(manager); // creating our hook with CREATE2
         require(address(counter) == hookAddress, "CounterScript: hook address mismatch");
 
         // init your pool
@@ -78,17 +60,15 @@ contract TreasuryScript is Script, Deployers {
                         Currency.wrap(address(token1)),
                         3000, 60, IHooks(counter));
 
-        vm.broadcast();
         manager.initialize(key, SQRT_RATIO_1_1, ZERO_BYTES);
 
-        vm.broadcast();
         Treasury treasury = new Treasury(address(token0), address(token1), manager);
         ICounter(counter).setTreasury(address(treasury));
 
 
-        vm.broadcast();
         //Setting liquidity at the end here
         router.modifyPosition(key, IPoolManager.ModifyPositionParams(-6000, 6000, 500 ether), abi.encode(msg.sender));
+        vm.stopBroadcast();
     }
 
 }
