e8abfd4c0c55ad73298af0d757cb5f3dcbd09708
Adam Jackson <18746720+atj3097@users.noreply.github.com>
2023-10-19 20:26:08 -0500

    deployable script to mumbai

--- script/Treasury.s.sol
+++ script/Treasury.s.sol
@@ -22,7 +22,7 @@ import {Currency, CurrencyLibrary} from "@uniswap/v4-core/contracts/types/Curren
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract TreasuryScript is Script, Deployers {
-    address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C); // Mumbai Deployer from -> https://github.com/pcaversaccio/create2deployer
+    address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C); // Foundry deployer address
     IPoolManager manager = IPoolManager(0x5FF8780e4D20e75B8599A9C4528D8ac9682e5c89); //Pool Manager address on Mumbai 🦄
     using CurrencyLibrary for Currency;
 
@@ -32,17 +32,21 @@ contract TreasuryScript is Script, Deployers {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // deploying our router
+        // deploying our own router
         PoolModifyPositionTest router =
         new PoolModifyPositionTest(IPoolManager(address(0x5FF8780e4D20e75B8599A9C4528D8ac9682e5c89)));
 
-        // Token addresses that I deployed on my own beforehand
-        IERC20 token0 = IERC20(0x2EFf4B88Cfa00180409A5cF2f248CBF6Af4e22D6);
-        IERC20 token1 = IERC20(0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97);
+        // deploying custom tokens, or you can replace this with MockERC20({ insert address }), you need to be able to mint those tokens though
+        MockERC20 token0 = new MockERC20("TESTUSDC", "USDC", 18);
+        MockERC20 token1 = new MockERC20("MEDELLIN", "MDE", 18);
 
-        //approving router to spend tokens to add liquidity later
-        token0.approve(address(router), 10);
-        token1.approve(address(router), 1000);
+        // minting tokens to msg.sender
+        token0.mint(msg.sender, 1000e18);
+        token1.mint(msg.sender, 1000e18);
+
+        //approving our router to spend our tokens to add liquidity later
+        token0.approve(address(router), 1000e18);
+        token1.approve(address(router), 1000e18);
 
         //setting the flag for the hook that we will be using
         uint160 flags = uint160(Hooks.AFTER_SWAP_FLAG);
@@ -65,12 +69,10 @@ contract TreasuryScript is Script, Deployers {
         manager.initialize(key, SQRT_RATIO_1_1, ZERO_BYTES);
 
         Treasury treasury = new Treasury(address(token0), address(token1), manager);
-        console2.log("Pt. 3");
         ICounter(counter).setTreasury(address(treasury));
 
-
-        //Setting liquidity at the end here
-        router.modifyPosition(key, IPoolManager.ModifyPositionParams(-6000, 6000, 500), abi.encode(msg.sender));
+        //Setting liquidity at the end here to setup our pool here
+        router.modifyPosition(key, IPoolManager.ModifyPositionParams(-6000, 6000, 500 ether), abi.encode(msg.sender));
         vm.stopBroadcast();
     }
 
